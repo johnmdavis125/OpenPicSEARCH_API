2df11682ba1c99f1fb19207c16c1f993ac71bc76
John Davis <johnmdavis125@gmail.com>
2022-05-05 16:45:19 -0400

    configure nodemon. Install express dotenv mongoose dependencies. Wire up basic express server and configure remote mongoDB instance via atlas

---
+++ server.js
@@ -0,0 +1,35 @@
+// dotenv
+require('dotenv').config()
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// express
+const express = require('express'); 
+const app = express(); 
+
+// mongoose
+const mongoose = require('mongoose'); 
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}); 
+const db = mongoose.connection; 
+db.on('open', ()=>{
+    console.log('OpenPicSearchAPI is connected to Mongo')
+});
+
+// Middleware //
+// app.use(cors());
+// app.use(express.json());
+// const path = require('path'); 
+// const cors = require('cors'); 
+
+// Cxn Test
+app.get('/', (req, res)=>{
+    res.send('Back is connected')
+});
+
+// Listener
+app.listen(PORT, ()=>{
+    console.log('DEVCON4 backend is awake and listening', PORT)
+});
